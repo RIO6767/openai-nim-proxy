6d06949c372e561b94f606fedb976a2a434196ca
RIO6767 <sushma251986@gmail.com>
2026-07-30 14:12:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -95,8 +95,8 @@ app.post('/v1/chat/completions', async (req, res) => {
     const nimRequest = {
       model: nimModel,
       messages: messages,
-      temperature: temperature || 0.8,
-      max_tokens: max_tokens || 16000,
+      temperature: temperature || 0.7,
+      max_tokens: max_tokens || 15000,
       extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
       stream: stream || false
     };
